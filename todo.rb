03c27267b65cdef951e6da00510b317606880a36
Adam Nawrocki <adam.nawroc@gmail.com>
2016-01-26 07:20:19 -0500

    progress

--- todo.rb
+++ todo.rb
@@ -1,7 +1,23 @@
 class List
+  def initialize (description)
+    @all_tasks = []
+    end
+
+    def add_task(tasks)
+    @all_tasks << task
+    end
+
+    def show_all_tasks
+    @all_tasks
+    end
 end
 
 class Task
+  attr_reader :description
   def initialize(description)
     @description = description
+  end
 end
+
+thing = Task.new("blah blah")
+puts thing
