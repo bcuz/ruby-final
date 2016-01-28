d10e09d4e4a05818fbb4214aea703922e342d62d
Adam Nawrocki <adam.nawroc@gmail.com>
2016-01-28 07:09:24 -0500

    save adding to an external file

--- todo.rb
+++ todo.rb
@@ -23,25 +23,28 @@ class Task
 end
 
 first_list = List.new
-my_list = List.new
 
-File.open("test.txt").each do |line|
-  my_list.add_task(Task.new(line))
-end
-
-def show(first_list)
-  first_list.show_all_tasks.each do |task|
-    puts task.description
-  end
-end
+# def show(first_list)
+#   first_list.show_all_tasks.each do |task|
+#     puts task.description
+#   end
+# end
 
 case command
-when "add"
-  task_obj = Task.new (task_string)
-  first_list.add_task(task_obj)
+  when "add"
+    task_obj = Task.new (task_string)
+    first_list.add_task(task_obj)
+end
+
+File.open("test.txt", "a") do |line|
+    first_list.show_all_tasks.each do |task|
+    line.puts "\r" + task.description
+  end
 end
 
 # is there a way to save the program? so i can add during one run
 # of the program and then view on another. Using a case
 # might be easier once using text files is incorporated
-show(my_list)
+
+# might be better to always print to a file...
+# show(my_list)
