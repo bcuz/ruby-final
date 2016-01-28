3c3e0bd2cb91f476d35f58fea074a7e1ead9928f
Adam Nawrocki <adam.nawroc@gmail.com>
2016-01-28 07:14:25 -0500

    add print command

--- todo.rb
+++ todo.rb
@@ -24,27 +24,18 @@ end
 
 first_list = List.new
 
-# def show(first_list)
-#   first_list.show_all_tasks.each do |task|
-#     puts task.description
-#   end
-# end
-
 case command
   when "add"
     task_obj = Task.new (task_string)
     first_list.add_task(task_obj)
-end
 
-File.open("test.txt", "a") do |line|
-    first_list.show_all_tasks.each do |task|
-    line.puts "\r" + task.description
+    File.open("test.txt", "a") do |line|
+      first_list.show_all_tasks.each do |task|
+      line.puts "\r" + task.description
+      end
+    end
+  when "print"
+    File.open("test.txt").each do |line|
+    puts line
   end
 end
-
-# is there a way to save the program? so i can add during one run
-# of the program and then view on another. Using a case
-# might be easier once using text files is incorporated
-
-# might be better to always print to a file...
-# show(my_list)
