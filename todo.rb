6184f4c1d9c2a92144f4ed111b88860b1a0cce92
Adam Nawrocki <adam.nawroc@gmail.com>
2016-02-03 07:34:03 -0500

    start of done case with ability to check if item is in list

--- todo.rb
+++ todo.rb
@@ -91,6 +91,21 @@ case command
   # another way to go is to put a weird character in front of the
   # completed stuff. Then strip that character(s) away when I want to
   # update the item. I like that idea
+  counter = 0
+  first_list.show_all_tasks.each do |item|
+    if item.description == task_string
+      item.description = "*** " + task_string
+      counter += 1
+    end
+
+  end
+
+  if counter < 1
+    puts "That item ain't even exist brah"
+  end
+
+  write_to_file("test.txt", first_list)
+
   when "delete"
 
     counter = 0
